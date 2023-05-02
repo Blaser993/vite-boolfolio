b1e8a8dd95da41cdb87bebfccf7d95ad5ea7d93b
Blaser993 <corradodepinto.design@gmail.com>
2023-05-02 16:46:08 +0200

    PUSH PRE TICKET

---
+++ src/components/Projects.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p v-for="project in projects" :key="project.id">
+            {{ projects.title }}
+        </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+            projects:[]
+        }
+    },
+    methods: {
+        fetchProjects() {
+            axios.get('127.0.0.1:8000/api/projects')
+            .then(res => {
+                console.log(res)
+                const {results} = res.data
+                this.projects = results
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    },
+    mounted() {
+        this.fetchProjects()
+    },
+}
+
+</script>
